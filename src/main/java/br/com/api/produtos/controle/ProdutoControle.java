fcf3eb60a8e4a1dc2d4cb37597717bbd636ecfbf
miguellopezpatricio <ml.patri@gmail.com>
2022-09-24 15:11:37 +0200

    Creado CRUD completo API backend Spring

--- src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -1,16 +1,37 @@
 package br.com.api.produtos.controle;
 
 import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.modelo.RespostaModelo;
 import br.com.api.produtos.servico.ProdutoServico;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") // Para solucionar problema CORS al solicitar desde frontend de puerto 3000
 public class ProdutoControle {
 
     @Autowired
     private ProdutoServico produtoServico;
+
+
+    @DeleteMapping("/remover/{codigo}")
+    public ResponseEntity<RespostaModelo> remover(@PathVariable long codigo){
+        return produtoServico.remover(codigo);
+    }
+
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo produtoModelo){
+        return produtoServico.cadastrarAlterar(produtoModelo, "alterar");
+
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo produtoModelo){
+        return produtoServico.cadastrarAlterar(produtoModelo, "cadastrar");
+
+    }
 
     @GetMapping("/listar")
     public Iterable<ProdutoModelo> listar(){
--- src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -2,8 +2,12 @@ package br.com.api.produtos.servico;
 
 
 import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.modelo.RespostaModelo;
 import br.com.api.produtos.repositorio.ProdutoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,10 +16,55 @@ public class ProdutoServico {
     @Autowired
     private ProdutoRepositorio produtoRepositorio;
 
+    @Autowired
+    private RespostaModelo respostaModelo;
+
+
+    // Método para listar todos os produtos
     public Iterable<ProdutoModelo> listar(){
         return produtoRepositorio.findAll();
     }
 
+
+    //Método para cadastrar ou alterar produtos
+    public ResponseEntity<?> cadastrarAlterar(ProdutoModelo produtoModelo, String acao){
+
+        if(produtoModelo.getNome().equals("")){
+            respostaModelo.setMensagem("O nome do produto é obrigatório");
+            return new ResponseEntity<RespostaModelo>(respostaModelo, HttpStatus.BAD_REQUEST);
+
+        } else if(produtoModelo.getMarca().equals("")){
+            respostaModelo.setMensagem("O nome da marca é obrigatorio");
+            return new ResponseEntity<RespostaModelo>(respostaModelo, HttpStatus.BAD_REQUEST);
+
+        }
+          else{
+              if(acao.equals("cadastrar")){
+                  return new ResponseEntity<ProdutoModelo>(produtoRepositorio.save(produtoModelo), HttpStatus.CREATED);
+
+              } else {
+                  return new ResponseEntity<ProdutoModelo>(produtoRepositorio.save(produtoModelo), HttpStatus.OK);
+
+              }
+
+          }
+
+
+
+
+    }
+
+
+    // Método para remover produtos
+    public ResponseEntity<RespostaModelo> remover(long codigo){
+
+        produtoRepositorio.deleteById(codigo);
+
+        respostaModelo.setMensagem("O produto foi removido con sucesso!");
+
+        return new ResponseEntity<RespostaModelo>(respostaModelo, HttpStatus.OK);
+
+    }
 
 
 
